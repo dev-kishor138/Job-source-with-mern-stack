0d20f0b2b8f18aa6c1a61e69916393f4d99b873d
Kishor Mahmud <dev.kishor138@gmail.com>
2023-05-26 18:38:20 +0600

    setup the project  and landing page complete

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Landing from "./Pages/Landing";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Landing></Landing>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/Pages/Landing.jsx
@@ -0,0 +1,68 @@
+import Logo from "../assets/images/logo.svg";
+import hero from "../assets/images/main.svg";
+import styled from "styled-components";
+
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <img src={Logo} alt="Job" className="logo" />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            Job <span>Tracking</span> App
+          </h1>
+          <p>
+            I'm baby wayfarers hoodie next level taiyaki brooklyn cliche blue
+            bottle single-origin coffee chia. Aesthetic post-ironic venmo,
+            quinoa lo-fi tote bag adaptogen everyday carry meggings +1 brunch
+            narwhal. Login/Register
+          </p>
+          <button className="btn btn-hero">Login/Register</button>
+        </div>
+        <img src={hero} alt="" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.main`
+  nav {
+    width: var(--fluid-width);
+    max-width: var(--max-width);
+    margin: 0 auto;
+    height: var(--nav-height);
+    display: flex;
+    align-items: center;
+  }
+  .page {
+    min-height: calc(100vh - var(--nav-height));
+    display: grid;
+    align-items: center;
+    margin-top: -3rem;
+  }
+  h1 {
+    font-weight: 700;
+    span {
+      color: var(--primary-500);
+    }
+  }
+  p {
+    color: var(--grey-600);
+  }
+  .main-img {
+    display: none;
+  }
+  @media (min-width: 992px) {
+    .page {
+      grid-template-columns: 1fr 1fr;
+      column-gap: 3rem;
+    }
+    .main-img {
+      display: block;
+    }
+  }
+`;
+
+export default Landing;
