ebeb74e9373328e7de1f9c9da3592d5d4c0ae8da
Kishor Mahmud <dev.kishor138@gmail.com>
2023-05-27 22:03:10 +0600

    register page design

--- client/src/Pages/Register/Register.jsx
+++ client/src/Pages/Register/Register.jsx
@@ -1,10 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import Logo from "../../assets/images/logo.svg";
+import Wrapper from "../../assets/wrappers/RegisterPage";
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  isMember: true,
+};
 
 const Register = () => {
+  const [values, setValues] = useState(initialState);
+
+  const handleChange = (e) => {
+    console.log(e.target);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
   return (
-    <div>
-      <h3>this is register page</h3>
-    </div>
+    <Wrapper className="full-page">
+      <form className="form" onSubmit={onSubmit}>
+        <img src={Logo} alt="Logo" />
+        <h3>Login</h3>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
+            name
+          </label>
+          <input
+            type="text"
+            value={values.name}
+            name="name"
+            onChange={handleChange}
+            className="form-input"
+          />
+        </div>
+        <button type="submit" className="btn btn-block">
+          Submit
+        </button>
+      </form>
+    </Wrapper>
   );
 };
 
